2837a4647bac166657f52d999a37833f1c1adc14
KaranVikas <karanvikas15@gmail.com>
2025-09-14 18:21:57 -0400

    Refactor URL patterns and clean up settings file by removing unnecessary whitespace

--- ch22/app1/urls.py
+++ ch22/app1/urls.py
@@ -1,8 +1,7 @@
 
-from django.contrib import admin
 from django.urls import path
 from app1.views import all_data
 
 urlpatterns = [
-    path("all/", all_data, name='all_data'),
+  path('all/', all_data, name='all_data'),
 ]
--- ch22/student/urls.py
+++ ch22/student/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("student/", include('student.urls'))
+    path('student/', include('student.urls')),
+    # path("student/", include('student.urls'))
 ]
+
