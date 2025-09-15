b7d9ef7bd5ceaed768d93fcb44883ff619aa7719
KaranVikas <karanvikas15@gmail.com>
2025-09-15 12:48:19 -0400

    Add student app integration with views, templates, and migrations

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path
-from student.views import all_data 
+from student import views
 
 urlpatterns = [
-    path('all/', all_data, name='all_data'),
+    path('cl1', views.MyClassView1.as_view(), name='myclassview'),
+    path('cl2', views.MyClassView2.as_view(), name='myclassview2'),
+    path('cl3', views.MyClassView3.as_view(name='Rahul'), name='myclassview3'),
+    path('chcl3', views.MychildClassView3.as_view(), name='mychildClassview3'),
+    path('homecl', views.HomeClassView.as_view() , name='homeclassview'),
+    path('aboutcl', views.HomeClassView.as_view() , name='aboutclassview'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,5 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from student.models import Profile
 
 # Create your views here.
-def all_data(req):
-  return render(req, 'student/all.html')
+class MyClassView1(View):
+  def get(self, request):
+    return HttpResponse("Hello Class based view")
+  
+class MyClassView2(View):
+  def get(self, request):
+    return HttpResponse("<h1>Class based view </h1>")
+
+class MyClassView3(View):
+  name = 'Sonam'
+  def get(self, request):
+    return HttpResponse(self.name)
+  
+class MychildClassView3(MyClassView3):
+  def get(self, request):
+    return HttpResponse(self.name)
+  
+class HomeClassView(View):
+  def get(self, request):
+    return render(request , 'student/all.html')
+  
+class AboutClassView(View):
+  def get(self, request):
+    context = {'msg': 'Welcome to mastering Django '}
+    return render(request , 'student/all_html', context)
+  
+
+  
+  
+
